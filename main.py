907683dddd887b325b7fdd34e86c7d7b5bdd578f
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-02-26 11:35:37 -0500

    Move files based on atom count

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import filter.excel as excel
 import filter.tags as tags
 import filter.min_distance as min_distance
 import filter.format as format
+import filter.supercell_size as supercell_size
 import filter.info as info
 import filter.occupancy as occupancy
 
@@ -14,15 +15,16 @@ def main():
         "1": "Move files based on unsupported CIF format",
         "2": "Move files based on unreasonable distance",
         "3": "Move files based on tags",
-        "4": "Copy files based on atomic occupancy and mixing",
-        "5": "Get file info in the folder",
-        "6": "Check CIF folder content against Excel file"
+        "4": "Move files based on supercell atom count",
+        "5": "Copy files based on atomic occupancy and mixing",
+        "6": "Get file info in the folder",
+        "7": "Check CIF folder content against Excel file"
     }
 
     for key, value in options.items():
         print(f"[{key}] {value}")
     
-    choice = input("Enter your choice (1-6): ")
+    choice = input("Enter your choice (1-7): ")
 
     if choice in options:
         print(f"You have chosen: {options[choice]}\n")
@@ -42,16 +44,20 @@ def main():
     elif choice == '3':
         tags.move_files_based_on_tags(script_directory)
         
-    # 4. Copy files based on atomic occupancy and atomic mixing
+    # 4. Relocate CIF based the number of atoms in the supercell
     elif choice == '4':
+        supercell_size.move_files_based_on_supercell_size(script_directory)
+
+    # 5. Copy files based on atomic occupancy and atomic mixing
+    elif choice == '5':
         occupancy.copy_files_based_on_atomic_occupancy_mixing(script_directory)
 
     # 6. Get info on the supercell
-    elif choice == '5':
+    elif choice == '6':
         info.get_CIF_files_info(script_directory)
     
-    # 6. Check missing files against Excel sheet
-    elif choice == '6':
+    # 7. Check missing files against Excel sheet
+    elif choice == '7':
         excel.get_new_Excel_with_matching_entries(script_directory)
 
     
